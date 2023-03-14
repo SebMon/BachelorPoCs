2a5de3cb39e7b34a5b61e772c00b2aa337a51a4c
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2023-03-14 16:33:51 +0100

    Now using references properly

--- benchmark-algorithms/src/main.rs
+++ benchmark-algorithms/src/main.rs
@@ -3,17 +3,17 @@ use thousands::Separable;
 
 mod sieve_of_atkin;
 
-const PRIME_LIMIT: usize= 10_000_000;
+const PRIME_LIMIT: usize= 100_000_000;
 
 fn main() {
     let start = Instant::now();
-    let primes = sieve_of_atkin::get_primes(PRIME_LIMIT);
+    let _primes = sieve_of_atkin::get_primes(&PRIME_LIMIT);
     let duration = start.elapsed();
     // Uncomment below loop to print 
     // Printing does not affect the timer, but will have an effect on 
     // profiling/measuring from the outside.
-    //for prime in primes {
-    //    print!("{} ", prime);
+    //for prime in _primes {
+    //    print!("{} ", prime.separate_with_commas());
     //}
     //println!("");
     println!("It took {} milliseconds to calculate primes under {}", duration.as_millis().separate_with_commas(), PRIME_LIMIT.separate_with_commas());
--- benchmark-algorithms/src/sieve_of_atkin.rs
+++ benchmark-algorithms/src/sieve_of_atkin.rs
@@ -1,10 +1,10 @@
 
-pub fn get_primes(limit: usize) -> Vec<usize>{
+pub fn get_primes(limit: &usize) -> Vec<usize>{
     let mut primes = Vec::new();
-    if limit > 2 {
+    if *limit > 2 {
         primes.push(2);
     };
-    if limit > 3 {
+    if *limit > 3 {
         primes.push(3);
     };
 
@@ -12,27 +12,27 @@ pub fn get_primes(limit: usize) -> Vec<usize>{
 
     let mut x: usize = 1;
 
-    while x * x <= limit {
+    while x * x <= *limit {
 
         let mut y: usize = 1;
-        while y * y <= limit {
+        while y * y <= *limit {
 
             let n = (4 * x * x) + (y * y); //125
-            if n<=limit && (n % 12 == 1 || n % 12 == 5) {
+            if n<= *limit && (n % 12 == 1 || n % 12 == 5) {
                 // If sieve[n] is false, set to true. If true, set to false
                 sieve[n] ^= true;
             }
 
             let n = (3 * x * x) + (y * y);
-            if n <= limit && n % 12 == 7 {
+            if n <= *limit && n % 12 == 7 {
                 // If sieve[n] is false, set to true. If true, set to false
                 sieve[n] ^= true;
             }
 
             let n: usize = (((3 * x * x) as isize) - ((y * y) as isize)) as usize;
-            if n <= limit && x > y && n % 12 == 11 {
+            if n <= *limit && x > y && n % 12 == 11 {
                 // If sieve[n] is false, set to true. If true, set to false
-                sieve[n as usize] ^= true;
+                sieve[n] ^= true;
             }
 
             y += 1;
@@ -42,10 +42,10 @@ pub fn get_primes(limit: usize) -> Vec<usize>{
     }
 
     let mut r = 5;
-    while r * r <= limit {
+    while r * r <= *limit {
         if sieve[r] {
             let mut i = r * r;
-            while i <= limit {
+            while i <= *limit {
                 sieve[i] = false;
                 i += r * r;
             }
@@ -53,7 +53,7 @@ pub fn get_primes(limit: usize) -> Vec<usize>{
         r += 1;
     }
 
-    for a in 5..=limit {
+    for a in 5..=*limit {
         if sieve[a] {
             primes.push(a);
         }
