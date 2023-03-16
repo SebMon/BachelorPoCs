b95f8375d7bcadd99589091390556a2ccd5c4dc7
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2023-03-16 17:56:20 +0100

    Adding needleman protein alignment. Super fast though, so not great to use to test. May have been a waste of time

--- benchmark-algorithms/src/main.rs
+++ benchmark-algorithms/src/main.rs
@@ -1,9 +1,12 @@
 use std::time::Instant;
 use thousands::Separable;
 
-mod sieve_of_atkin;
+use crate::needleman_wunsch::calculate_score;
 
-const PRIME_LIMIT: usize= 100_000_000;
+mod sieve_of_atkin;
+mod needleman_wunsch;
+
+const PRIME_LIMIT: usize= 100_000;
 
 fn main() {
     let start = Instant::now();
@@ -12,9 +15,46 @@ fn main() {
     // Uncomment below loop to print 
     // Printing does not affect the timer, but will have an effect on 
     // profiling/measuring from the outside.
-    //for prime in _primes {
-    //    print!("{} ", prime.separate_with_commas());
-    //}
-    //println!("");
+    
+    for prime in _primes {
+        print!("{} ", prime.separate_with_commas());
+    }
+    println!("");
     println!("It took {} milliseconds to calculate primes under {}", duration.as_millis().separate_with_commas(), PRIME_LIMIT.separate_with_commas());
+    
+    let protein_x = vec!['G', 'C', 'A', 'A', 'T',
+        'G', 'T', 'G', 'T', 'C', 
+        'A', 'C', 'G', 'G', 'T', 
+        'T', 'G', 'G', 'A', 'A', 
+        'A', 'A', 'G', 'C', 'C', 
+        'A', 'A', 'C', 'G', 'A', 
+        'T', 'G', 'G', 'C', 'T', 
+        'G', 'T', 'A', 'C', 'C', 
+        'G', 'C', 'A', 'C', 'A', 
+        'A', 'T', 'G', 'T', 'T'];
+    let protein_y = vec!['C', 'G', 'A', 'G', 'A', 
+        'G', 'C', 'T', 'A', 'G', 
+        'G', 'C', 'A', 'T', 'T', 
+        'A', 'G', 'A', 'C', 'A', 
+        'T', 'A', 'A', 'G', 'T', 
+        'C', 'T', 'A', 'A', 'C', 
+        'A', 'G', 'A', 'G', 'G', 
+        'T', 'G', 'C', 'T', 'C', 
+        'T', 'T', 'C', 'T', 'C', 
+        'A', 'C', 'C', 'T', 'C'];
+
+    let start = Instant::now();
+    let (protein_x, protein_y) = calculate_score(&protein_x, &protein_y);
+    let duration = start.elapsed();
+    println!("It took {} milliseconds to the protein stuff", duration.as_millis().separate_with_commas());
+
+    for c in protein_x {
+        print!("{}", c);
+    }
+    println!("");
+    for c in protein_y {
+        print!("{}", c);
+    }
+    println!("");
+    
 }
---
+++ benchmark-algorithms/src/needleman_wunsch.rs
@@ -0,0 +1,83 @@
+use std::vec;
+
+const GAP_PENALTY: isize = 1;
+const MATCH_AWARD: isize = 1;
+const MISMATCH_PENALTY: isize = 1;
+
+pub fn calculate_score(x: &Vec<char>, y: &Vec<char>) -> (Vec<char>, Vec<char>,) {
+    let nx = x.len();
+    let ny = y.len();
+
+    let mut f: Vec<Vec<isize>> = vec![vec![0; ny + 1]; nx + 1];
+
+    // Fill top row and left most column
+    for i in 0..f.len() {
+        f[i][0] = GAP_PENALTY * -(i as isize);
+    }
+    for i in 0..f.len() {
+        f[0][i] = GAP_PENALTY * -(i as isize);
+    }
+
+    let mut p = vec![vec![0; ny + 1]; nx + 1];
+    for i in 0..p.len() {
+        p[i][0] = 3;
+    }
+    for i in 0..p.len() {
+        p[0][i] = 4;
+    }
+
+    let mut t = vec![0; 3];
+    for i in 0..nx {
+        for j in 0..ny {
+            if x[i] == y[j] {
+                t[0] = f[i][j] + MATCH_AWARD;
+            } else {
+                t[0] = f[i][j] -  MISMATCH_PENALTY;
+            }
+            t[1] = f[i][j + 1] - GAP_PENALTY;
+            t[2] = f[i + 1][j] - GAP_PENALTY;
+
+            let max_value = *t.iter().max().unwrap();
+            f[i + 1][j + 1] = max_value;
+
+            if t[0] == max_value {
+                p[i + 1][j + 1] += 2;
+            }
+            if t[1] == max_value {
+                p[i + 1][j + 1] += 3;
+            }
+            if t[2] == max_value {
+                p[i + 1][j + 1] += 4;
+            }
+        }
+    }
+
+
+    let mut i = nx;
+    let mut j = ny;
+
+    let mut rx = Vec::new();
+    let mut ry = Vec::new();
+
+    while i > 0 || j > 0 {
+        if [2, 5, 6, 9].contains(&p[i][j]) {
+            rx.push(x[i - 1]);
+            ry.push(y[j - 1]);
+            i -= 1;
+            j -= 1;
+        } else if [3, 5, 7, 9].contains(&p[i][j]) {
+            rx.push(x[i - 1]);
+            ry.push('-');
+            i -= 1;
+        } else if [4, 6, 7, 9].contains(&p[i][j]) {
+            rx.push('-');
+            ry.push(y[j - 1]);
+            j -= 1;
+        }
+    }
+
+    rx.reverse();
+    ry.reverse();
+    
+    (rx, ry)
+}
