00774a6798c7314afa0152d2d0050cbc967e1344
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2023-03-16 18:31:39 +0100

    Now calculating alignment for proteins of length 2000 - adjustable with constants.

--- benchmark-algorithms/src/main.rs
+++ benchmark-algorithms/src/main.rs
@@ -1,12 +1,14 @@
 use std::time::Instant;
 use thousands::Separable;
+use rand::seq::SliceRandom;
 
 use crate::needleman_wunsch::calculate_score;
 
 mod sieve_of_atkin;
 mod needleman_wunsch;
 
-const PRIME_LIMIT: usize= 100_000;
+const PRIME_LIMIT: usize= 20_000_000;
+const PROTEIN_SIZE: u32 = 2_000;
 
 fn main() {
     let start = Instant::now();
@@ -16,38 +18,30 @@ fn main() {
     // Printing does not affect the timer, but will have an effect on 
     // profiling/measuring from the outside.
     
+    /*
     for prime in _primes {
         print!("{} ", prime.separate_with_commas());
     }
     println!("");
+    */
     println!("It took {} milliseconds to calculate primes under {}", duration.as_millis().separate_with_commas(), PRIME_LIMIT.separate_with_commas());
-    
-    let protein_x = vec!['G', 'C', 'A', 'A', 'T',
-        'G', 'T', 'G', 'T', 'C', 
-        'A', 'C', 'G', 'G', 'T', 
-        'T', 'G', 'G', 'A', 'A', 
-        'A', 'A', 'G', 'C', 'C', 
-        'A', 'A', 'C', 'G', 'A', 
-        'T', 'G', 'G', 'C', 'T', 
-        'G', 'T', 'A', 'C', 'C', 
-        'G', 'C', 'A', 'C', 'A', 
-        'A', 'T', 'G', 'T', 'T'];
-    let protein_y = vec!['C', 'G', 'A', 'G', 'A', 
-        'G', 'C', 'T', 'A', 'G', 
-        'G', 'C', 'A', 'T', 'T', 
-        'A', 'G', 'A', 'C', 'A', 
-        'T', 'A', 'A', 'G', 'T', 
-        'C', 'T', 'A', 'A', 'C', 
-        'A', 'G', 'A', 'G', 'G', 
-        'T', 'G', 'C', 'T', 'C', 
-        'T', 'T', 'C', 'T', 'C', 
-        'A', 'C', 'C', 'T', 'C'];
+
+
+    // Generate random protein strings
+    let mut protein_x: Vec<char> = Vec::new();
+    let mut protein_y: Vec<char> = Vec::new();
+    let possible_values = vec!['A','T','G','C'];
+    for _ in 0..PROTEIN_SIZE {
+        protein_x.push(possible_values.choose(&mut rand::thread_rng()).unwrap().clone());
+        protein_y.push(possible_values.choose(&mut rand::thread_rng()).unwrap().clone());
+    }
 
     let start = Instant::now();
     let (protein_x, protein_y) = calculate_score(&protein_x, &protein_y);
     let duration = start.elapsed();
-    println!("It took {} milliseconds to the protein stuff", duration.as_millis().separate_with_commas());
+    println!("It took {} milliseconds to calculate protein alignments with proteins of length {}", duration.as_millis().separate_with_commas(), PROTEIN_SIZE.separate_with_commas());
 
+    /*
     for c in protein_x {
         print!("{}", c);
     }
@@ -56,5 +50,6 @@ fn main() {
         print!("{}", c);
     }
     println!("");
+    */
     
 }
